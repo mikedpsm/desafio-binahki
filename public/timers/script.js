5510e8ef6048100bef919aec3a0471d91bfd98eb
Maicon Douglas <11251303+mikedpsm@users.noreply.github.com>
2022-04-04 01:50:56 -0300

    Removed timer as object

--- public/timers/script.js
+++ public/timers/script.js
@@ -3,7 +3,6 @@ const time_el = document.getElementById("timer1");
 const start_btn = document.getElementById("start1");
 const stop_btn = document.getElementById("stop1");
 const reset_btn = document.getElementById("reset1");
-const close_btn = document.getElementById("close1");
 const idInput = document.getElementById("idInput1");
 
 const addTimer_btn = document.getElementById("addTimer");
@@ -13,23 +12,9 @@ const label = document.querySelector(".input-container label");
 let seconds = 0;
 let interval = null;
 
-// Reference for timer id controllers
 let timerNumber = 2;
 
-// Arrays
-const secondsArr = [];
-const intervalArr = [];
-
-secondsArr.push(seconds);
-console.log(secondsArr);
-
-function runInterval() {
-  console.log("run interval");
-  const interval = setInterval(runTimer, 1000);
-  intervalArr.push(interval);
-}
-
-function runTimer() {
+function timer() {
   seconds++;
 
   let hrs = Math.floor(seconds / (60 * 60));
@@ -41,53 +26,28 @@ function runTimer() {
   if (hrs < 10) hrs = "0" + hrs;
 
   time_el.innerText = `${hrs}:${mins}:${secs}`;
-  console.log(secondsArr);
 }
 
-const objTimer1 = {
-  id: timerNumber - 1,
-  isRunning: false,
-  interval: function () {
-    console.log("interval rodando");
-    setInterval(this.timer, 1000);
-  },
-  killInterval: function () {
-    clearInterval(this.interval);
-  } /*
-  seconds: 0,
-  timer: function () {
-    console.log("timer");
-    this.seconds++;
+function start() {
+  if (interval) {
+    return;
+  } else if (!interval && idInput.value === "") {
+    alertInput();
+    return;
+  }
 
-    let hrs = Math.floor(this.seconds / (60 * 60));
-    let mins = Math.floor((this.seconds - hrs * 3600) / 60);
-    let secs = this.seconds % 60;
+  removeAlert();
+  interval = setInterval(timer, 1000);
+}
 
-    if (secs < 10) secs = "0" + secs;
-    if (mins < 10) mins = "0" + mins;
-    if (hrs < 10) hrs = "0" + hrs;
+function stop() {
+  clearInterval(interval);
+  seconds = 0;
+}
 
-    time_el.innerText = `${hrs}:${mins}:${secs}`;
-  },*/,
-  start: function () {
-    if (this.isRunning) {
-      console.log("nao passou");
-      return;
-    } else if (!this.isRunning && idInput.value === "") {
-      alertInput();
-      return;
-    }
+function finish() {}
 
-    console.log("passou");
-    removeAlert();
-    runInterval();
-    this.isRunning = true;
-  },
-  stop: function () {
-    killInterval();
-    this.isRunning = false;
-    this.interval = null;
-  },
+/*
   finish: function () {
     if (idInput.value === "") {
       alertInput();
@@ -103,10 +63,10 @@ const objTimer1 = {
     this.seconds = 0;
   },
 };
-
-start_btn.addEventListener("click", objTimer1.start);
-stop_btn.addEventListener("click", objTimer1.stop);
-reset_btn.addEventListener("click", objTimer1.finish);
+*/
+start_btn.addEventListener("click", start);
+stop_btn.addEventListener("click", stop);
+reset_btn.addEventListener("click", finish);
 
 addTimer_btn.addEventListener("click", createTimer);
 
@@ -123,11 +83,22 @@ function removeAlert() {
 // Dynamic HTML
 
 function createTimer() {
-  console.log("criou");
   const newTime = document.createElement("div");
   newTime.classList.add("time");
   newTime.textContent = "00:00:00";
   newTime.setAttribute("id", `timer${timerNumber}`);
+
+  const newForm = document.createElement("form");
+
+  const newInputId = document.createElement("input");
+  newForm.append(newInputId);
+  newInputId.setAttribute("type", "text");
+  newInputId.setAttribute("placeholder", "ID");
+
+  const newInputSample = document.createElement("input");
+  newForm.append(newInputSample);
+  newInputSample.setAttribute("type", "number");
+  newInputSample.setAttribute("placeholder", "Sample Number #");
 
   const newControls = document.createElement("div");
   newControls.classList.add("controls");
@@ -138,24 +109,20 @@ function createTimer() {
   newStopBtn.innerText = "STOP";
   const newFinishBtn = document.createElement("button");
   newFinishBtn.innerText = "FINISH";
-  const newCloseBtn = document.createElement("button");
-  newCloseBtn.innerText = "X";
 
   newStartBtn.addEventListener("click", start);
   newStopBtn.addEventListener("click", stop);
   newFinishBtn.addEventListener("click", finish);
-  newCloseBtn.addEventListener("click", close);
 
   newControls.append(newStartBtn);
   newControls.append(newStopBtn);
   newControls.append(newFinishBtn);
-  newControls.append(newCloseBtn);
 
   const newTimer = document.createElement("div");
   newTimer.classList.add("watch");
   newTimer.append(newTime);
   newTimer.append(newControls);
-  // newTimer.setAttribute("id", `start${timerNumber}`);
+  newTimer.append(newForm);
 
   watchContainer.append(newTimer);
 
