c60a4da9c908d62a5b71167035ef3fd203f6cdba
Maicon Douglas <11251303+mikedpsm@users.noreply.github.com>
2022-04-06 15:15:43 -0300

    Date formatted

--- public/timerDashboard/dashboard.js
+++ public/timerDashboard/dashboard.js
@@ -24,11 +24,28 @@ const loadDashboard = async function () {
               new Date(startday).getDate() < 10
                 ? new Date(startday).getDate().toString().padStart(2, "0")
                 : new Date(startday).getDate()
-            }</span>    
-            <span>Session end: ${endday.slice(8, 11)}/${endday.slice(
-            4,
-            7
-          )}/</span>    
+            }/${
+            new Date(startday).getMonth() < 10
+              ? new Date(startday).getMonth().toString().padStart(2, "0")
+              : new Date(startday).getMonth()
+          }/${
+            new Date(startday).getFullYear() < 10
+              ? new Date(startday).getFullYear().toString()
+              : new Date(startday).getFullYear()
+          }</span>    
+            <span>Session end: ${
+              new Date(endday).getDate() < 10
+                ? new Date(endday).getDate().toString().padStart(2, "0")
+                : new Date(endday).getDate()
+            }/${
+            new Date(endday).getMonth() < 10
+              ? new Date(endday).getMonth().toString().padStart(2, "0")
+              : new Date(endday).getMonth()
+          }/${
+            new Date(endday).getFullYear() < 10
+              ? new Date(endday).getFullYear().toString()
+              : new Date(endday).getFullYear()
+          }</span>    
             <span>Time active: ${(timeactive / 60 / 60).toFixed(2)} hours</span>
         </div>
     `
